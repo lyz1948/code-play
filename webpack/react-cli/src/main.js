60a25d45b84ac032b977c9c61f88d6c15b49901c
lyz <lvqinbo@163.com>
2019-06-12 11:54:12 +0800

    react base

---
+++ webpack/react-cli/src/main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <div>
+    <h1>Hello, React!</h1>
+    <p>webpack</p>
+  </div>,
+  document.getElementById('root')
+)
